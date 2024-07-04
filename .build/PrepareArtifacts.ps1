c684c223509d8d52db690c96b4e80a8c9e6b668a
Jared Goodwin <translucency@outlook.com>
2024-07-04 14:45:45 -0700

    Update PrepareArtifacts.ps1

--- .build/PrepareArtifacts.ps1
+++ .build/PrepareArtifacts.ps1
@@ -12,8 +12,8 @@ $WorkDir = $env:SYSTEM_DEFAULTWORKINGDIRECTORY
 
 [System.IO.Directory]::CreateDirectory("$ArtifactsDir\Server\wwwroot\downloads\deltas") | Out-Null
 [System.IO.Directory]::CreateDirectory("$ArtifactsDir\Server\wwwroot\downloads\signatures") | Out-Null
-Copy-Item -Path "$ArtifactsDir\Server\wwwroot\downloads\deltas\*" -Destination "$WorkDir\_ControlR\Server\wwwroot\downloads\deltas" -Recurse -Force
-Copy-Item -Path "$ArtifactsDir\Server\wwwroot\downloads\signatures\*" -Destination "$WorkDir\_ControlR\Server\wwwroot\downloads\signatures" -Recurse -Force
+Copy-Item -Path "$ArtifactsDir\Server\wwwroot\downloads\deltas\" -Destination "$WorkDir\_ControlR\Server\wwwroot\downloads\deltas\" -Recurse -Force
+Copy-Item -Path "$ArtifactsDir\Server\wwwroot\downloads\signatures\" -Destination "$WorkDir\_ControlR\Server\wwwroot\downloads\signatures\" -Recurse -Force
 
 &"$WorkDir\_ControlR\CreateDelta\CreateDelta.ps1" -DownloadsFolder "$WorkDir/_ControlR/Server/wwwroot/downloads" -OldFilePath "$ArtifactsDir\Server\wwwroot\downloads\win-x86\ControlR.Streamer.zip" -NewFilePath "$WorkDir\_ControlR\Server\wwwroot\downloads\win-x86\ControlR.Streamer.zip" -BaseDeltaName "windows-streamer" -OctodiffPath $OctodiffPath
 
