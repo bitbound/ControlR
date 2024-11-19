2a844f6ad3e1a705c3024d594f9f153169876d9d
Jared Goodwin <translucency@outlook.com>
2024-11-18 18:49:29 -0800

    Add drive percentages.  Move branding on layout.

--- Libraries/ControlR.Libraries.Shared/Models/Drive.cs
+++ Libraries/ControlR.Libraries.Shared/Models/Drive.cs
@@ -5,25 +5,57 @@ namespace ControlR.Libraries.Shared.Models;
 [MessagePackObject]
 public record Drive
 {
-    [MsgPackKey]
-    [JsonConverter(typeof(JsonStringEnumConverter))]
-    public DriveType DriveType { get; set; }
+  [MsgPackKey]
+  [JsonConverter(typeof(JsonStringEnumConverter))]
+  public DriveType DriveType { get; set; }
 
-    [MsgPackKey]
-    public string RootDirectory { get; set; } = string.Empty;
+  [MsgPackKey]
+  public string RootDirectory { get; set; } = string.Empty;
 
-    [MsgPackKey]
-    public string Name { get; set; } = string.Empty;
+  [MsgPackKey]
+  public string Name { get; set; } = string.Empty;
 
-    [MsgPackKey]
-    public string DriveFormat { get; set; } = string.Empty;
+  [MsgPackKey]
+  public string DriveFormat { get; set; } = string.Empty;
 
-    [MsgPackKey]
-    public double FreeSpace { get; set; }
+  [MsgPackKey]
+  public double FreeSpace { get; set; }
 
-    [MsgPackKey]
-    public double TotalSize { get; set; }
+  [MsgPackKey]
+  public double TotalSize { get; set; }
 
-    [MsgPackKey]
-    public string VolumeLabel { get; set; } = string.Empty;
+  [MsgPackKey]
+  public string VolumeLabel { get; set; } = string.Empty;
+
+  [IgnoreMember]
+  [JsonIgnore]
+  public double UsedSpace => TotalSize - FreeSpace;
+
+  [IgnoreMember]
+  [JsonIgnore]
+  public string FreeSpacePercentFormatted
+  {
+    get
+    {
+      if (TotalSize > 0)
+      {
+        return $"{Math.Round(FreeSpace / TotalSize * 100, 2)}%";
+      }
+      return "Unknown";
+    }
+  }
+
+  [IgnoreMember]
+  [JsonIgnore]
+  public string UsedSpacePercentFormatted
+  {
+    get
+    {
+      if (TotalSize > 0)
+      {
+        return $"{Math.Round(UsedSpace / TotalSize * 100, 2)}%";
+      }
+      return "Unknown";
+    }
+  }
 }
