d05f52ea531114de7a75ac338e913b8c12899a2c
Jared Goodwin <translucency@outlook.com>
2026-07-14 18:32:01 -0700

    Simplify DistributedLock by removing manual semaphore management and optimizing acquisition.

--- Libraries/ControlR.Libraries.Shared/Services/DistributedLock.cs
+++ Libraries/ControlR.Libraries.Shared/Services/DistributedLock.cs
@@ -2,27 +2,21 @@ using System.Collections.Concurrent;
 
 namespace ControlR.Libraries.Shared.Services;
 
-
 public interface IDistributedLock
 {
   Task<IDisposable> AcquireLock(string key, CancellationToken cancellationToken);
 }
 
-public class DistributedLock : IDistributedLock
+public sealed class DistributedLock : IDistributedLock
 {
-  private readonly ConcurrentDictionary<string, SemaphoreSlim> _semaphores = [];
+  /// <summary>
+  /// Maximum count is bound by manually-defined keys, so we don't have to worry about unbound growth.
+  /// </summary>
+  private readonly ConcurrentDictionary<string, SemaphoreSlim> _locks = [];
 
   public async Task<IDisposable> AcquireLock(string key, CancellationToken cancellationToken)
   {
-    var semaphore = _semaphores.GetOrAdd(key, _ => new SemaphoreSlim(1, 1));
-    await semaphore.WaitAsync(cancellationToken).ConfigureAwait(false);
-    return new CallbackDisposable(() =>
-    {
-      semaphore.Release();
-      if (semaphore.CurrentCount > 0)
-      {
-        _semaphores.TryRemove(new KeyValuePair<string, SemaphoreSlim>(key, semaphore));
-      }
-    });
+    var semaphore = _locks.GetOrAdd(key, _ => new SemaphoreSlim(1, 1));
+    return await semaphore.AcquireLockAsync(cancellationToken);
   }
 }
